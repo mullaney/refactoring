50276817de3ba2bfff7f70ce20e718b5d5af8e66
Kevin Kelly <kvinklly@gmail.com>
2020-01-23 13:37:05 -0600

    extract totalVolumeCredits()

--- src/statement.js
+++ src/statement.js
@@ -7,14 +7,21 @@ function statement (invoice, plays) {
     totalAmount += amountFor(perf);
   }
 
-  let volumeCredits = 0;
-  for (let perf of invoice.performances) {
-    volumeCredits += volumeCreditsFor(perf);
-  }
+  let volumeCredits = totalVolumeCredits();
 
   result += `Amount owed is ${usd(totalAmount)}\n`;
   result += `You earned ${volumeCredits} credits\n`;
   return result;
+
+  /* NESTED FUNCTIONS */
+
+  function totalVolumeCredits() {
+    let volumeCredits = 0;
+    for (let perf of invoice.performances) {
+      volumeCredits += volumeCreditsFor(perf);
+    }
+    return volumeCredits;
+  }
 
   function amountFor(aPerformance) {
     let result = 0;
