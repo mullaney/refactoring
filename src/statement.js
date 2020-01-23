aba72d5879b782b19b4a6ca38a4e2b9373efae25
Kevin Kelly <kvinklly@gmail.com>
2020-01-23 16:59:36 -0600

    move volumeCreditsFor

--- src/statement.js
+++ src/statement.js
@@ -8,6 +8,7 @@ function statement (invoice, plays) {
     const result = Object.assign({}, aPerformance);
     result.play = playFor(result);
     result.amount = amountFor(result);
+    result.volumeCredits = volumeCreditsFor(result);
     return result;
   }
 
@@ -36,6 +37,15 @@ function statement (invoice, plays) {
     }
     return result;
   }
+
+  function volumeCreditsFor(aPerformance) {
+    let result = 0;
+    result += Math.max(aPerformance.audience - 30, 0);
+    if ("comedy" === aPerformance.play.type) {
+      result += Math.floor(aPerformance.audience / 5);
+    }
+    return result;
+  }
 }
 
 function renderPlainText (data) {
@@ -52,7 +62,7 @@ function renderPlainText (data) {
   function totalVolumeCredits() {
     let result = 0;
     for (let perf of data.performances) {
-      result += volumeCreditsFor(perf);
+      result += perf.volumeCredits;
     }
     return result;
   }
@@ -61,15 +71,6 @@ function renderPlainText (data) {
     let result = 0;
     for (let perf of data.performances) {
       result += perf.amount;
-    }
-    return result;
-  }
-
-  function volumeCreditsFor(aPerformance) {
-    let result = 0;
-    result += Math.max(aPerformance.audience - 30, 0);
-    if ("comedy" === aPerformance.play.type) {
-      result += Math.floor(aPerformance.audience / 5);
     }
     return result;
   }
