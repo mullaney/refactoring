088eeb5c2c0ebdd155cb194b64f21add775eeb8c
Kevin Kelly <kvinklly@gmail.com>
2020-01-30 22:03:00 -0600

    move logic from amountFor to performance calculator

--- src/createStatementData.js
+++ src/createStatementData.js
@@ -19,6 +19,10 @@ export default function createStatementData (invoice, plays) {
   function playFor(aPerformance) {
     return plays[aPerformance.playID]
   }
+
+  function amountFor(aPerformance) {
+    return new PerformanceCalculator(aPerformance, playFor(aPerformance)).amount;
+  }
 }
 
 class PerformanceCalculator {
@@ -48,28 +52,6 @@ class PerformanceCalculator {
     }
     return result;
   }
-}
-
-function amountFor(aPerformance) {
-  let result = 0;
-  switch (aPerformance.play.type) {
-    case "tragedy":
-      result = 40000;
-      if (aPerformance.audience > 30) {
-        result += 1000 * (aPerformance.audience - 30);
-      }
-      break;
-    case "comedy":
-      result = 30000;
-      if (aPerformance.audience > 20) {
-        result += 1000 + 500 * (aPerformance.audience - 20);
-      }
-      result += 300 * aPerformance.audience;
-      break;
-    default:
-      throw new Error(`unknown type: ${aPerformance.play.type}`);
-  }
-  return result;
 }
 
 function volumeCreditsFor(aPerformance) {
