fce9bac12a6fbb807eb57c8669362feaf440d1c5
Kevin Kelly <kvinklly@gmail.com>
2020-01-31 16:12:21 -0600

    extract applyShipping function

--- src/priceOrder.js
+++ src/priceOrder.js
@@ -4,6 +4,11 @@ function priceOrder(product, quantity, shippingMethod) {
   const basePrice = product.basePrice * quantity;
   const discount = Math.max(quantity - product.discountThreshold, 0)
     * product.basePrice * product.discountRate;
+  const price = applyShipping(basePrice, shippingMethod, quantity, discount);
+  return price;
+}
+
+function applyShipping(basePrice, shippingMethod, quantity, discount) {
   const shippingPerCase = (basePrice > shippingMethod.discountThreshold)
     ? shippingMethod.discountFee
     : shippingMethod.feePerCase;
