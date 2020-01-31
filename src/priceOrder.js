fc1c30bf0e70c94e4500011424ec2943d14daf3d
Kevin Kelly <kvinklly@gmail.com>
2020-01-31 16:21:31 -0600

    move quantity into data object

--- src/priceOrder.js
+++ src/priceOrder.js
@@ -4,16 +4,16 @@ function priceOrder(product, quantity, shippingMethod) {
   const basePrice = product.basePrice * quantity;
   const discount = Math.max(quantity - product.discountThreshold, 0)
     * product.basePrice * product.discountRate;
-  const priceData = { basePrice };
-  const price = applyShipping(priceData, shippingMethod, quantity, discount);
+  const priceData = { basePrice, quantity };
+  const price = applyShipping(priceData, shippingMethod, discount);
   return price;
 }
 
-function applyShipping(priceData, shippingMethod, quantity, discount) {
+function applyShipping(priceData, shippingMethod, discount) {
   const shippingPerCase = (priceData.basePrice > shippingMethod.discountThreshold)
     ? shippingMethod.discountFee
     : shippingMethod.feePerCase;
-  const shippingCost = quantity * shippingPerCase;
+  const shippingCost = priceData.quantity * shippingPerCase;
   const price = priceData.basePrice - discount + shippingCost;
   return price;
 }
